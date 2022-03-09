84d1d6632e43285938a608675bf0d0a1fde1477a
Mohammed Moazzam Khan <kmohdmoazzam70@gmail.com>
2022-03-09 13:10:53 +0530

    Added Login Register

--- backend/api/controller/userController.js
+++ backend/api/controller/userController.js
@@ -5,7 +5,10 @@ const CareTaker = require('../model/careTaker');
 exports.registerUser = async (req, res, next) => {
     try {
         const { name, email, contact, password } = req.body
-
+        if( !name || !email || !contact || !password){
+            res.status(200).send({ status: 400, message: "Wrong Credentials" });
+            return;
+        }
         const userExist = await CareTaker.findOne({ email: email });
 
         if (userExist) {
@@ -22,6 +25,28 @@ exports.registerUser = async (req, res, next) => {
         const careTaker = await createNewCareTaker.save();
         res.status(200).send({ status: 200, message: "User Registered Successfully" });
         return;
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.loginUser = async(req,res,next) => {
+    try {
+        const { email, password } = req.body
+        const user = await CareTaker.findOne({ email: email })
+        if (user == null) {
+            res.status(200).send({ status: 400, message: "Invalid Username or Password" })
+        } else {
+            if (email == user.email && user.password == password) {
+                res.status(200).json({
+                    status: 200,
+                    message: "User Login Successfully",
+                    data: {
+                        user: user
+                    }
+                })
+            }
+        }
     } catch (error) {
         next(error)
     }
